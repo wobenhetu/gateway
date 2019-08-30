c9714b8b2805e50e0da03f5c8c30ff6f013bb250
dengfenwu <dengfenwu@ysstech.com>
2019-08-30 14:53:42 +0800

    tokenfilter恢复

--- gateway/src/main/java/com/yss/gateway/filter/TokenFilter.java
+++ gateway/src/main/java/com/yss/gateway/filter/TokenFilter.java
@@ -2,7 +2,6 @@ package com.yss.gateway.filter;
 
 import com.yss.gateway.config.PropertiesConfig;
 import com.yss.gateway.entity.AuthToken;
-import com.yss.gateway.entity.Users;
 import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
@@ -14,7 +13,6 @@ import reactor.core.publisher.Mono;
 
 import java.security.Principal;
 import java.util.List;
-import java.util.Map;
 
 public class TokenFilter implements GlobalFilter, Ordered {
 
@@ -23,29 +21,11 @@ public class TokenFilter implements GlobalFilter, Ordered {
 
     @Override
     public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
-        //List<String> all = propertiesConfig.handleUri();
+        List<String> all = propertiesConfig.handleUri();
         String uri = exchange.getRequest().getPath().pathWithinApplication().value();
         System.out.println("dfw==访问的url为：" + uri);
 
-        if(uri.endsWith("/deng/info")){
-            String username = exchange.getRequest().getQueryParams().get("username").toString();
-            String password = exchange.getRequest().getQueryParams().get("password").toString();
-            String token = exchange.getRequest().getQueryParams().get("token").toString();
-
-            Principal user = parseIdentity(token,username,password);
-            if (user == null) {   //如果token认证失败user为null
-                return exchange.getResponse().setComplete();
-            }
-
-            for (Map.Entry<String, String> entry : Users.USERS_MAP.entrySet()) {
-                if (entry.getKey().equals(username) && entry.getValue().equals(password)) {
-                    return chain.filter(exchange);
-                }
-            }
-        }
-        return chain.filter(exchange);
-
-        /*for (String url : all) {
+        for (String url : all) {
             //过滤不需要拥有token的连接
             if (uri.endsWith(url)) {
                 System.out.println("不需要拥有token的uri：" + uri);
@@ -58,8 +38,8 @@ public class TokenFilter implements GlobalFilter, Ordered {
             exchange.getResponse().setStatusCode(HttpStatus.BAD_REQUEST);
             System.out.println("需要传token，否则请求无法通过");
             return exchange.getResponse().setComplete();
-        }*/
-        //return chain.filter(exchange);
+        }
+        return chain.filter(exchange);
 
 
     }
@@ -70,15 +50,14 @@ public class TokenFilter implements GlobalFilter, Ordered {
     }
 
 
-
-    private Principal parseIdentity(String token,String username,String password) {
-        if(!StringUtils.isEmpty(token) && !StringUtils.isEmpty(username) && !StringUtils.isEmpty(password)){
+    private Principal parseIdentity(String token, String username, String password) {
+        if (!StringUtils.isEmpty(token) && !StringUtils.isEmpty(username) && !StringUtils.isEmpty(password)) {
             AuthToken authToken = new AuthToken();
             authToken.setToken(token);
             authToken.setUsername(username);
             authToken.setPassword(password);
             return authToken;
-        }else{
+        } else {
             return null;
         }
 
